8e232f21e42e88588e70dcdf70cad83046eddccc
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-08-25 18:18:07 +0200

    association

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -2,4 +2,7 @@ import cate from "./cate.js";
 import prod from "./prod.js";
 import sequelize from "../connect.js";
 
+prod.belongsTo(cate);
+cate.hasMany(prod);
+
 export default { cate, prod, sequelize };
