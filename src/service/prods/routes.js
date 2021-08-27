911b2f1b00380a34e3959a2acbef9736cb08f294
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-08-27 14:28:32 +0200

    limit

--- src/service/prods/routes.js
+++ src/service/prods/routes.js
@@ -14,6 +14,7 @@ router
   .get(async (req, res, next) => {
     try {
       const product = await prods.findAll({
+        limit: 2,
         include: [categs, { model: comments, include: user }],
       });
 
